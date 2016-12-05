fec838716b9823787960497af6d69d4cf21d32ce
Philippe Vidal <philippe.vidal.80@gmail.com>
2016-12-05 11:11:41 +0100

    RDK-check_script_presence.py: Correction du schéma du script pour getops.

--- RDK-check_script_presence.py
+++ RDK-check_script_presence.py
@@ -4,30 +4,39 @@ import time
 import sys
 import logging
 import psutil
+import getopt
 
-script = ''
+def main(argv):
 
-try:
-   opts, args = getopt.getopt(argv,"hs:",["help","script="])
-except getopt.GetoptError:
-   print 'check_script_presence.py -s <script>'
-   sys.exit(2)
-for opt, arg in opts:
-   if opt == '-h':
+   script = ''
+   
+   try:
+      opts, args = getopt.getopt(argv,"hs:",["help","script="])
+   except getopt.GetoptError:
       print 'check_script_presence.py -s <script>'
-      sys.exit()
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'check_script_presence.py -s <script>'
+         sys.exit()
+      elif opt in ("-s", "--script"):
+         script = arg
+   
+   if script == '':
+         print 'check_script_presence.py -s <script>'
+         sys.exit(1)
+   
+   for pid in psutil.pids():
+       p = psutil.Process(pid)
+       if p.name() == "python" and len(p.cmdline()) > 1 and script in p.cmdline()[1]:
+           sys.stdout.write(time.strftime("%Y/%m/%d-%H:%M:%S")+': ERROR - SCRIPT '+script+' ALREADY RUNNING.\n')
+           sys.stdout.flush()
+           sys.exit("ERROR - SCRIPT "+script+" ALREADY RUNNING.\n")
+   
+   sys.stdout.write(time.strftime("%Y/%m/%d-%H:%M:%S")+': INFO - SCRIPT '+script+' NOT RUNNING.\n')
+   sys.stdout.flush()
+   sys.exit(0)
 
-if script == '':
-      print 'check_script_presence.py -s <script>'
-      sys.exit(1)
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-for pid in psutil.pids():
-    p = psutil.Process(pid)
-    if p.name() == "python" and len(p.cmdline()) > 1 and scirpt in p.cmdline()[1]:
-        sys.stdout.write(time.strftime("%Y/%m/%d-%H:%M:%S")+': ERROR - SCRIPT '+script+' ALREADY LAUNCHED.\n')
-        sys.stdout.flush()
-        sys.exit("ERROR - SCRIPT "+script+" ALREADY LAUNCHED.\n")
-
-sys.stdout.write(time.strftime("%Y/%m/%d-%H:%M:%S")+': INFO - SCRIPT '+scirpt+' NOT RUNNING.\n')
-sys.stdout.flush()
-sys.exit(0)
